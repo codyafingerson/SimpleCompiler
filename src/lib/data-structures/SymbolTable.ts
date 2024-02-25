32456c51559564879938032ecb69eeafc469b02b
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2024-02-25 00:35:37 -0700

    Implement symbol table data structure

---
+++ src/lib/data-structures/SymbolTable.ts
@@ -0,0 +1,55 @@
+class SymbolTable<K, V> {
+    private readonly keys: K[];
+    private readonly values: V[];
+    private size: number;
+
+    constructor() {
+        this.keys = [];
+        this.values = [];
+        this.size = 0;
+    }
+
+    public put(key: K, value: V): void {
+        if(this.contains(key)) {
+            this.values[this.keys.indexOf(key)] = value;
+        } else {
+            this.keys.push(key);
+            this.values.push(value);
+            this.size++;
+        }
+    }
+
+    public get(key: K): V {
+        return this.values[this.keys.indexOf(key)];
+    }
+
+    public delete(key: K): void {
+        if(this.contains(key)) {
+            this.values.splice(this.keys.indexOf(key), 1);
+            this.keys.splice(this.keys.indexOf(key), 1);
+            this.size--;
+        }
+    }
+
+    public contains(key: K): boolean {
+        return this.keys.includes(key);
+    }
+
+    public isEmpty(): boolean {
+        return this.size === 0;
+    }
+
+    public getSize(): number {
+        return this.size;
+    }
+
+    public getKeys(): K[] {
+        return this.keys;
+    }
+
+    public getValues(): V[] {
+        return this.values;
+    }
+}
+
+export default SymbolTable;
