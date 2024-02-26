d1808d2e5ebb179db254f74b17391d1a6464cde6
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2024-02-25 23:29:20 -0700

    Disable code input as this has not been implemented yet.
    Also prevents the default form submission event.

--- src/pages/CompilerPage.tsx
+++ src/pages/CompilerPage.tsx
@@ -1,20 +1,24 @@
 import Container from "../components/Container.tsx";
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 import Button from "../components/Button.tsx";
 import CodeInput from "../components/CodeInput.tsx";
 
 export default function CompilerPage() {
-    const [sourceCode, setSourceCode] = useState("");
+    const [sourceCode, setSourceCode] = useState("Not implemented yet... stay tuned!");
 
     const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
         setSourceCode(e.target.value);
     }
 
+    const onSubmit = (e: FormEvent) => {
+        e.preventDefault();
+    }
+
     return (
         <Container>
-            <form className="flex flex-col space-y-4">
-                <CodeInput value={sourceCode} onChangeFunction={onChange} />
-                <Button type="submit" text="Compile"/>
+            <form className="flex flex-col space-y-4" onSubmit={onSubmit}>
+                <CodeInput value={sourceCode} onChangeFunction={onChange} isDisabled={true}/>
+                <Button type="submit" text="Compile" />
             </form>
         </Container>
     );
--- src/pages/ParserPage.tsx
+++ src/pages/ParserPage.tsx
@@ -1,19 +1,23 @@
 import Container from "../components/Container.tsx";
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 import Button from "../components/Button.tsx";
 import CodeInput from "../components/CodeInput.tsx";
 
 export default function ParserPage() {
-    const [sourceCode, setSourceCode] = useState("");
+    const [sourceCode, setSourceCode] = useState("Not implemented yet... stay tuned!");
 
     const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
         setSourceCode(e.target.value);
     }
 
+    const onSubmit = (e: FormEvent) => {
+        e.preventDefault();
+    }
+
     return (
         <Container>
-            <form className="flex flex-col space-y-4">
-                <CodeInput value={sourceCode} onChangeFunction={onChange} />
+            <form className="flex flex-col space-y-4" onSubmit={onSubmit}>
+                <CodeInput value={sourceCode} onChangeFunction={onChange} isDisabled={true}/>
                 <Button type="submit" text="Parse"/>
             </form>
         </Container>
