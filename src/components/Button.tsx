08b369ebc3ec2bbff68b9e6061ac6e2b99f52865
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2024-02-25 23:28:30 -0700

    Add isDisabled to disable element if needed

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,10 +2,11 @@ interface ButtonProps {
     type: "submit" | "reset" | "button";
     text: string;
     onClick?: () => void;
+    isDisabled?: boolean;
 }
 
-export default function Button({ type, text, onClick }: ButtonProps) {
+export default function Button({ type, text, onClick, isDisabled }: ButtonProps) {
     return (
-        <button type={type} className="w-32 p-2 bg-violet-400 text-white rounded hover:bg-violet-300 hover:text-gray-800" onClick={onClick}>{text}</button>
+        <button type={type} className="w-32 p-2 bg-violet-400 text-white rounded hover:bg-violet-300 hover:text-gray-800" onClick={onClick} disabled={isDisabled}>{text}</button>
     );
 }
--- src/components/CodeInput.tsx
+++ src/components/CodeInput.tsx
@@ -3,12 +3,13 @@ import {ChangeEvent, FC} from "react";
 interface CodeInputProps {
     value: string;
     onChangeFunction: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+    isDisabled?: boolean;
 }
 
 // Your CodeInput component would look something like this:
-const CodeInput: FC<CodeInputProps> = ({ value, onChangeFunction }) => {
+const CodeInput: FC<CodeInputProps> = ({ value, onChangeFunction, isDisabled }) => {
     return (
-        <textarea value={value} onChange={onChangeFunction} className="p-4 rounded-lg bg-gray-700 text-white h-96 focus:border-gray-800 font-mono" />
+        <textarea value={value} onChange={onChangeFunction} className="p-4 rounded-lg bg-gray-700 text-white h-96 focus:border-gray-800 font-mono" disabled={isDisabled}/>
     );
 }
 
