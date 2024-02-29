d5f0a35aeeb92e643f084cb724a7444479cc0a21
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2024-02-29 14:12:45 -0700

    Change onChange function to match new CodeInput component props.

--- src/pages/RuntimePage.tsx
+++ src/pages/RuntimePage.tsx
@@ -1,14 +1,14 @@
 import Container from "../components/Container.tsx";
-import {ChangeEvent, FormEvent, useState} from "react";
+import { FormEvent, useCallback, useState } from "react";
 import Button from "../components/Button.tsx";
 import CodeInput from "../components/CodeInput.tsx";
 
 export default function RuntimePage() {
     const [sourceCode, setSourceCode] = useState("Not implemented yet... stay tuned!");
 
-    const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        setSourceCode(e.target.value);
-    }
+    const onChange = useCallback((val: string) => {
+        setSourceCode(val);
+    }, []);
 
     const onSubmit = (e: FormEvent) => {
         e.preventDefault();
@@ -17,7 +17,7 @@ export default function RuntimePage() {
     return (
         <Container>
             <form className="flex flex-col space-y-4" onSubmit={onSubmit}>
-                <CodeInput value={sourceCode} onChangeFunction={onChange} isDisabled={true}/>
+                <CodeInput value={sourceCode} onChangeFunction={onChange} />
                 <Button type="submit" text="Execute"/>
             </form>
         </Container>
